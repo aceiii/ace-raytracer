f4a04c4f9b5c5f88c5899408eddf23ae950483f0
Borin Ouch <borin.ouch@gmail.com>
2024-02-01 01:47:53 -0800

    Limit recursion depth

--- camera.h
+++ camera.h
@@ -9,7 +9,8 @@ class camera {
 public:
     double  aspect_ratio = 1.0;
     int     image_width = 100;
-    int     samples_per_pixel = 100;
+    int     samples_per_pixel = 10;
+    int     max_depth = 10;
 
     void render(const hittable& world) {
         initialize();
@@ -22,7 +23,7 @@ public:
                 colour pixel_colour(0, 0, 0);
                 for (int sample = 0; sample < samples_per_pixel; sample += 1) {
                     ray r = get_ray(i, j);
-                    pixel_colour += ray_colour(r, world);
+                    pixel_colour += ray_colour(r, max_depth, world);
                 }
                 write_colour(std::cout, pixel_colour, samples_per_pixel);
             }
@@ -61,11 +62,16 @@ private:
         pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
     }
 
-    colour ray_colour(const ray& r, const hittable& world) const {
+    colour ray_colour(const ray& r, int depth, const hittable& world) const {
         hit_record rec;
+
+        if (depth <= 0) {
+            return colour(0, 0, 0);
+        }
+
         if (world.hit(r, interval(0, infinity), rec)) {
             vec3 direction = random_on_hemisphere(rec.normal);
-            return 0.5 * ray_colour(ray(rec.p, direction), world);
+            return 0.5 * ray_colour(ray(rec.p, direction), depth-1, world);
         }
 
         vec3 unit_direction = unit_vector(r.direction());
