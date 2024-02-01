84995bceed0f0ed3b1e251483108ab4a4930a2a8
Borin Ouch <borin.ouch@gmail.com>
2024-01-31 22:30:11 -0800

    Detect ray sphere intersection to draw a red sphere

--- main.cpp
+++ main.cpp
@@ -8,7 +8,20 @@ colour lerp(double a, const colour& start, const colour& end) {
     return (1.0 - a) * start + a * end;
 }
 
+bool hit_sphere(const point3& center, double radius, const ray& r) {
+    vec3 oc = r.origin() - center;
+    auto a = dot(r.direction(), r.direction());
+    auto b = 2.0 * dot(oc, r.direction());
+    auto c = dot(oc, oc) - radius * radius;
+    auto discriminant = b*b - 4*a*c;
+    return discriminant >= 0;
+}
+
 colour ray_colour(const ray& r) {
+    if (hit_sphere(point3(0, 0, -1), 0.5, r)) {
+        return colour(1, 0, 0);
+    }
+
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5 * (unit_direction.y() + 1.0);
     return lerp(a, colour(1.0, 1.0, 1.0), colour(0.5, 0.7, 1.0));
