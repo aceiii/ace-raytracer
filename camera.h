6c39eb9f838e919191dbd9c83e47679924bef9f2
Borin Ouch <borin.ouch@gmail.com>
2024-02-01 01:50:29 -0800

    Fix shadow acne problem

--- camera.h
+++ camera.h
@@ -69,7 +69,7 @@ private:
             return colour(0, 0, 0);
         }
 
-        if (world.hit(r, interval(0, infinity), rec)) {
+        if (world.hit(r, interval(0.001, infinity), rec)) {
             vec3 direction = random_on_hemisphere(rec.normal);
             return 0.5 * ray_colour(ray(rec.p, direction), depth-1, world);
         }
