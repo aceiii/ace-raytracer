39a7281fe2a09d87291178247703fe20dbe5b485
Borin Ouch <borin.ouch@gmail.com>
2024-02-01 01:17:43 -0800

    Add clamp method to interval

--- interval.h
+++ interval.h
@@ -17,6 +17,16 @@ public:
         return min < x && x < max;
     }
 
+    double clamp(double x) const {
+        if (x < min) {
+            return min;
+        }
+        if (x > max) {
+            return max;
+        }
+        return x;
+    }
+
     static const interval empty, universe;
 };
 
