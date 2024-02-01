422013c729f4c9c219918fc37e3d14166c6c7e85
Borin Ouch <borin.ouch@gmail.com>
2024-02-01 01:16:54 -0800

    Add random functions

--- rtweekend.h
+++ rtweekend.h
@@ -2,8 +2,10 @@
 #define __RTWEEKEND_H__
 
 #include <cmath>
+#include <cstdlib>
 #include <limits>
 #include <memory>
+#include <random>
 
 // Usings
 using std::shared_ptr;
@@ -17,6 +19,18 @@ const double pi = 3.1415926535897932385;
 // Utility functions
 inline double degrees_to_radius(double degrees) {
     return degrees * pi / 180.0;
+}
+
+inline double random_double() {
+    // returns a random real in [0,1)
+    static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+    static std::mt19937 generator;
+    return distribution(generator);
+}
+
+inline double random_double(double min, double max) {
+    // returns a random real in [min, max)
+    return min + (max - min) * random_double();
 }
 
 // Common headers
