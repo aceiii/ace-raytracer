9aab307cd0d3cd34503bd55ee0e913c2c0be1d7e
Borin Ouch <borin.ouch@gmail.com>
2024-02-02 22:13:06 -0800

    Convert to true lambertian reflections

--- camera.h
+++ camera.h
@@ -70,7 +70,7 @@ private:
         }
 
         if (world.hit(r, interval(0.001, infinity), rec)) {
-            vec3 direction = random_on_hemisphere(rec.normal);
+            vec3 direction = rec.normal + random_unit_vector();
             return 0.5 * ray_colour(ray(rec.p, direction), depth-1, world);
         }
 
