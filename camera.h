81d2b0de90a02f4bde5795fa537ea9451de0a7eb
Borin Ouch <borin.ouch@gmail.com>
2024-02-03 03:10:57 -0800

    Implement defocus blur (depth of field)

--- camera.h
+++ camera.h
@@ -14,6 +14,12 @@ public:
     int     max_depth = 10;
 
     double vfov = 90;
+    point3 lookfrom = point3(0, 0, -1); // center of camera
+    point3 lookat = point3(0, 0, 0); // target
+    vec3 vup = vec3(0, 1, 0); // camera-relative "up" direction
+
+    double defocus_angle = 0; // variation of angle of rays through each pixel
+    double focus_dist = 10; // distance from camera lookfrom to perfect focus
 
     void render(const hittable& world) {
         initialize();
@@ -41,30 +47,43 @@ private:
     point3  pixel00_loc;
     vec3    pixel_delta_u;
     vec3    pixel_delta_v;
+    vec3    u, v, w; // camera frame basis vectors
+    vec3    defocus_disk_u;
+    vec3    defocus_disk_v;
 
     void initialize() {
         image_height = static_cast<int>(image_width / aspect_ratio);
         image_height = (image_height < 1) ? 1 : image_height;
-        center = point3(0, 0, 0);
+        center = lookfrom;
 
         // Camera
-        auto focal_length = 1.0;
+        // auto focal_length = (lookfrom - lookat).length();
         auto theta = degrees_to_radians(vfov);
         auto h = tan(theta / 2);
-        auto viewport_height = 2 * h * focal_length;
+        auto viewport_height = 2 * h * focus_dist;
         auto viewport_width = viewport_height * (static_cast<double>(image_width) / image_height);
 
+        // calculate basis vectors for camera
+        w = unit_vector(lookfrom - lookat); // opposite direction of lookat
+        u = unit_vector(cross(vup, w)); // camera right vector
+        v = cross(w, u); // camera up vector
+
         // Vectors across and down the viewport edges
-        auto viewport_u = vec3(viewport_width, 0, 0);
-        auto viewport_v = vec3(0, -viewport_height, 0); // positive y is up
+        vec3 viewport_u = viewport_width * u;
+        vec3 viewport_v = viewport_height * -v; // positive y is up
 
         // Vectors from pixel to pixel across and down
         pixel_delta_u = viewport_u / image_width;
         pixel_delta_v = viewport_v / image_height;
 
         // Calculate upper left pixel location
-        auto viewport_upper_left = center - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
+        auto viewport_upper_left = center - (focus_dist * w) - viewport_u/2 - viewport_v/2;
         pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
+        // Calculate focus disk basis vectors
+        auto defocus_radius = focus_dist * tan(degrees_to_radians(defocus_angle / 2));
+        defocus_disk_u = u * defocus_radius;
+        defocus_disk_v = v * defocus_radius;
     }
 
     colour ray_colour(const ray& r, int depth, const hittable& world) const {
@@ -91,14 +110,21 @@ private:
 
     ray get_ray(int i, int j) const {
         // get randomly sampled camera ray for pixel at location i,j
+        // originate from camera defocus disk
 
         auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
         auto pixel_sample = pixel_center + pixel_sample_square();
 
-        auto ray_origin = center;
+        auto ray_origin = defocus_angle <= 0 ? center : defocus_disk_sample();
         auto ray_direction = pixel_sample - ray_origin;
 
         return ray(ray_origin, ray_direction);
+    }
+
+    point3 defocus_disk_sample() const {
+        // random point within defocus disk
+        auto p = random_in_unit_disk();
+        return center + (p[0] * defocus_disk_u) + (p[1] * defocus_disk_v);
     }
 
     vec3 pixel_sample_square() const {
--- main.cpp
+++ main.cpp
@@ -9,31 +9,28 @@
 int main() {
     hittable_list world;
 
-    // auto material_ground = make_shared<lambertian>(colour(0.8, 0.8, 0.0));
-    // auto material_center = make_shared<lambertian>(colour(0.1, 0.2, 0.5));
-    // auto material_left = make_shared<dieletric>(1.5);
-    // auto material_right = make_shared<metal>(colour(0.8, 0.6, 0.2), 0.0);
+    auto material_ground = make_shared<lambertian>(colour(0.8, 0.8, 0.0));
+    auto material_center = make_shared<lambertian>(colour(0.1, 0.2, 0.5));
+    auto material_left = make_shared<dieletric>(1.5);
+    auto material_right = make_shared<metal>(colour(0.8, 0.6, 0.2), 0.0);
 
-    // world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
-    // world.add(make_shared<sphere>(point3(0.0, 0.0, -1.0), 0.5, material_center));
-    // world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
-    // world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), -0.4, material_left));
-    // world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
-
-
-    auto R = cos(pi / 4);
-    auto material_left = make_shared<lambertian>(colour(0, 0, 1));
-    auto material_right = make_shared<lambertian>(colour(1, 0, 0));
-
-    world.add(make_shared<sphere>(point3(-R, 0, -1), R, material_left));
-    world.add(make_shared<sphere>(point3(R, 0, -1), R, material_right));
+    world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
+    world.add(make_shared<sphere>(point3(0.0, 0.0, -1.0), 0.5, material_center));
+    world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
+    world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), -0.4, material_left));
+    world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
 
     camera cam;
     cam.aspect_ratio = 16.0 / 9.0;
     cam.image_width = 800;
     cam.samples_per_pixel = 50;
     cam.max_depth = 50;
-    cam.vfov = 90;
+    cam.vfov = 20;
+    cam.lookfrom = point3(-2, 2, 1);
+    cam.lookat = point3(0, 0, -1);
+    cam.vup = vec3(0, 1, 0);
+    cam.defocus_angle = 10.0;
+    cam.focus_dist = 3.4;
     cam.render(world);
 
     return 0;
