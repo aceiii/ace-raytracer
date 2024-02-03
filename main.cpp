a5e5d345910bd4b149bf981758e784216bb29d8b
Borin Ouch <borin.ouch@gmail.com>
2024-02-02 22:12:33 -0800

    Increase image size and lower sample count

--- main.cpp
+++ main.cpp
@@ -12,8 +12,8 @@ int main() {
 
     camera cam;
     cam.aspect_ratio = 16.0 / 9.0;
-    cam.image_width = 400;
-    cam.samples_per_pixel = 100;
+    cam.image_width = 800;
+    cam.samples_per_pixel = 50;
     cam.max_depth = 50;
     cam.render(world);
 
