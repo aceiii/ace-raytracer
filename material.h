9e73f4b565ed4ea15f28f3ba60b972ca890cc895
Borin Ouch <borin.ouch@gmail.com>
2024-02-03 01:43:28 -0800

    Implement dieletric material

--- material.h
+++ material.h
@@ -35,17 +35,37 @@ private:
 
 class metal : public material {
 public:
-    metal(const colour& a): albedo(a) {};
+    metal(const colour& a, double f): albedo(a), fuzz(f < 1 ? f : 1) {};
 
-    bool scatter(const ray& r_in, const hit_record& rec, colour& attenuation, ray& scatterd) const override {
+    bool scatter(const ray& r_in, const hit_record& rec, colour& attenuation, ray& scattered) const override {
         auto reflected = reflect(unit_vector(r_in.direction()), rec.normal);
-        scatterd = ray(rec.p, reflected);
+        scattered = ray(rec.p, reflected + fuzz * random_unit_vector());
         attenuation = albedo;
-        return true;
+        return dot(scattered.direction(), rec.normal) > 0;
     }
 
 private:
     colour albedo;
+    double fuzz;
+};
+
+class dieletric : public material {
+public:
+    dieletric(double index_of_refraction) : ir(index_of_refraction) {}
+
+    bool scatter(const ray& r_in, const hit_record& rec, colour& attenuation, ray& scattered) const override {
+        attenuation = colour(1.0, 1.0, 1.0);
+        double refraction_ratio = rec.front_face ? (1.0 /ir) : ir;
+
+        vec3 unit_direction = unit_vector(r_in.direction());
+        vec3 refracted = refract(unit_direction, rec.normal, refraction_ratio);
+
+        scattered = ray(rec.p, refracted);
+        return true;
+    }
+
+private:
+    double ir; // index of refraction
 };
 
 #endif//__MATERIAL_H__
