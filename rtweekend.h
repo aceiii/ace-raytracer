cab63f65e6980067ac0a1eed4fb26e83544ce9ba
Borin Ouch <borin.ouch@gmail.com>
2024-02-03 12:56:03 -0800

    Seed random generator with acual random value

--- rtweekend.h
+++ rtweekend.h
@@ -23,8 +23,9 @@ inline double degrees_to_radians(double degrees) {
 
 inline double random_double() {
     // returns a random real in [0,1)
+    static std::random_device rd;
     static std::uniform_real_distribution<double> distribution(0.0, 1.0);
-    static std::mt19937 generator;
+    static std::mt19937 generator(rd());
     return distribution(generator);
 }
 
