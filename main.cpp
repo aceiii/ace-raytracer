33cffa469d1c41e69080588d1f72d1fce9e7ab24
Borin Ouch <borin.ouch@gmail.com>
2024-02-03 12:56:22 -0800

    Lower resolution and sample count for dev

--- main.cpp
+++ main.cpp
@@ -51,8 +51,8 @@ int main() {
 
     camera cam;
     cam.aspect_ratio = 16.0 / 9.0;
-    cam.image_width = 1200;
-    cam.samples_per_pixel = 500;
+    cam.image_width = 400;
+    cam.samples_per_pixel = 50;
     cam.max_depth = 50;
     cam.vfov = 20;
     cam.lookfrom = point3(13, 2, 3);
