63f0c64ef987649c11fe7ab468e7e41bbb1e659a
Borin Ouch <borin.ouch@gmail.com>
2024-02-11 22:49:57 -0800

    Add raygui, display rendering status in raygui status bar

--- raylib_window.cpp
+++ raylib_window.cpp
@@ -1,6 +1,14 @@
 #include "raylib_window.h"
 
 #include <raylib.h>
+#include <raygui.h>
+
+namespace {
+    tf::Taskflow taskflow;
+    tf::Future<void> future;
+
+    Camera3D cam3d;
+}
 
 static void set_logging_level(const std::string& level) {
     if (level == "trace") {
@@ -36,11 +44,80 @@ inline vec3 fromRaylibVector3(const Vector3& vec) {
     };
 }
 
+inline Color toRaylibColor(const pixel& px) {
+    return Color{ px.r, px.g, px.b, px.a };
+}
+
+bool update(tf::Executor &executor, camera &cam, const hittable_list& world, std::shared_ptr<bitmap> bmp) {
+    if (IsKeyPressed(KEY_ESCAPE)) {
+        return true;
+    }
+
+    if (IsKeyPressed(KEY_SPACE)) {
+        if (cam.rendering() && !cam.complete()) {
+            cam.cancel();
+        } else if (cam.complete()) {
+            bmp->clear();
+            cam.cancel();
+            future.wait();
+        } else {
+            bmp->clear();
+            cam.init();
+            future = executor.run(taskflow);
+        }
+    }
+
+    if (!cam.rendering()) {
+        UpdateCamera(&cam3d, CAMERA_FIRST_PERSON);
+        cam.lookat = fromRaylibVector3(cam3d.target);
+        cam.lookfrom = fromRaylibVector3(cam3d.position);
+        cam.vup = fromRaylibVector3(cam3d.up);
+        cam.vfov = cam3d.fovy;
+    }
+
+    return false;
+}
+
+inline void draw_rendered_bmp(std::shared_ptr<bitmap> bmp) {
+    for (int y = 0; y < GetScreenHeight(); y += 1) {
+        for (int x = 0; x < GetScreenWidth(); x += 1) {
+            auto &px = bmp->pixel_at(x, y);
+            if (px.a) {
+                DrawPixel(x, y, toRaylibColor(px));
+            }
+        }
+    }
+}
+
+void draw(tf::Executor &executor, camera &cam, const hittable_list& world, std::shared_ptr<bitmap> bmp) {
+    BeginDrawing();
+    ClearBackground(BLACK);
+
+    BeginMode3D(cam3d);
+    world.draw();
+    EndMode3D();
+
+    if (cam.rendering() || cam.complete()) {
+        draw_rendered_bmp(bmp);
+    }
+
+    auto status_text = "Idle";
+    if (cam.rendering() && !cam.complete()) {
+        status_text = "Rendering...";
+    } else if (cam.complete()) {
+        status_text = "Done";
+    }
+    GuiStatusBar({ 0, (float)GetScreenHeight() - 32, (float)GetScreenWidth(), 32 }, status_text);
+
+    DrawFPS(10, 10);
+
+    EndDrawing();
+}
+
 void raylib_window::run(camera& cam, const hittable_list& world, std::shared_ptr<bitmap> bmp) {
     set_logging_level(log_level);
 
     tf::Executor executor(num_threads);
-    tf::Taskflow taskflow;
 
     auto start_render = taskflow.emplace([&world, &cam, bmp](tf::Subflow subflow) {
         cam.render(world, subflow, bmp);
@@ -49,75 +126,19 @@ void raylib_window::run(camera& cam, const hittable_list& world, std::shared_ptr
     dimensions dims = cam.get_image_dimensions();
     InitWindow(dims.width, dims.height, "Ace Raytracer!");
 
-    Camera3D cam3d;
     cam3d.fovy = cam.vfov;
     cam3d.position = toRaylibVector3(cam.lookfrom);
     cam3d.target = toRaylibVector3(cam.lookat);
     cam3d.up = toRaylibVector3(cam.vup);
     cam3d.projection = CAMERA_PERSPECTIVE;
 
-    tf::Future<void> future;
+    bool forceSquaredChecked = true;
 
     while (!WindowShouldClose()) {
-        if (IsKeyPressed(KEY_ESCAPE)) {
+        if (update(executor, cam, world, bmp)) {
             break;
         }
-
-        if (IsKeyPressed(KEY_SPACE)) {
-            if (cam.rendering() && !cam.complete()) {
-                cam.cancel();
-            } else if (cam.complete()) {
-                bmp->clear();
-                cam.cancel();
-                future.wait();
-            } else {
-                bmp->clear();
-                cam.init();
-                future = executor.run(taskflow);
-            }
-        }
-
-        if (!cam.rendering()) {
-            UpdateCamera(&cam3d, CAMERA_FIRST_PERSON);
-            cam.lookat = fromRaylibVector3(cam3d.target);
-            cam.lookfrom = fromRaylibVector3(cam3d.position);
-            cam.vup = fromRaylibVector3(cam3d.up);
-            cam.vfov = cam3d.fovy;
-        }
-
-        BeginDrawing();
-        ClearBackground(BLACK);
-
-        BeginMode3D(cam3d);
-        world.draw();
-        EndMode3D();
-
-        if (cam.rendering() || cam.complete()) {
-            for (int y = 0; y < dims.height; y += 1) {
-                for (int x = 0; x < dims.width; x += 1) {
-                    auto &px = bmp->pixel_at(x, y);
-                    if (px.a) {
-                        Color color { px.r, px.g, px.b, px.a };
-                        DrawPixel(x, y, color);
-                    }
-                }
-            }
-        }
-
-        int font_size = 20;
-        if (cam.rendering() && !cam.complete()) {
-            DrawText("Rendering...", 10, dims.height - 10 - font_size, font_size, BLACK);
-            DrawText("Rendering...", 11, dims.height - 9 - font_size, font_size, RAYWHITE);
-        }
-
-        if (cam.complete()) {
-            DrawText("DONE", 10, dims.height - 10 - font_size, font_size, BLACK);
-            DrawText("DONE", 11, dims.height - 9 - font_size, font_size, RAYWHITE);
-        }
-
-        DrawFPS(10, 10);
-
-        EndDrawing();
+        draw(executor, cam, world, bmp);
     }
 
     cam.cancel();
