a0d2d169ce28c0576bec53a665c9a137a91d7462
Borin Ouch <borin.ouch@gmail.com>
2024-02-15 00:05:08 -0800

    Fix bug in vec3 class that was causing issues with bvh calculation

--- vec3.h
+++ vec3.h
@@ -20,8 +20,8 @@ public:
     inline double z() const { return e[2]; }
 
     inline vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
-    inline double operator[](int i) const { return e[1]; }
-    inline double& operator[](int i) { return e[1]; }
+    inline double operator[](int i) const { return e[i]; }
+    inline double& operator[](int i) { return e[i]; }
 
     vec3& operator+=(const vec3 &v) {
         e[0] += v.e[0];
