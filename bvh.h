f33d49f6cd1ca976a90c1f222e9a13b394cb5eed
Borin Ouch <borin.ouch@gmail.com>
2024-02-14 22:51:15 -0800

    Implement code for implementing tree of bvh nodes

--- bvh.h
+++ bvh.h
@@ -5,12 +5,41 @@
 #include "hittable.h"
 #include "hittable_list.h"
 
+#include <algorithm>
+
 class bvh_node : public hittable {
 public:
     bvh_node(const hittable_list& list) : bvh_node(list.objects, 0, list.objects.size()) {}
 
     bvh_node(const std::vector<shared_ptr<hittable>>& src_objects, size_t start, size_t end) {
-        // To be implemented later...
+        auto objects = src_objects; // create modifiable array of source objects
+
+        int axis = random_integer(0, 2);
+        auto comparator = (axis == 0) ? box_x_compare
+                        : (axis == 1) ? box_y_compare
+                                      : box_z_compare;
+
+        size_t object_span = end - start;
+
+        if (object_span == 1) {
+            left = right = objects[start];
+        } else if (object_span == 2) {
+            if (comparator(objects[start], objects[start + 1])) {
+                left = objects[start];
+                right = objects[start + 1];
+            } else {
+                left = objects[start + 1];
+                right = objects[start];
+            }
+        } else {
+            std::sort(objects.begin() + start, objects.begin() + end, comparator);
+
+            auto mid = start + object_span / 2;
+            left = make_shared<bvh_node>(objects, start, mid);
+            right = make_shared<bvh_node>(objects, mid, end);
+        }
+
+        bbox = aabb(left->bounding_box(), right->bounding_box());
     }
 
     bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
@@ -32,6 +61,22 @@ private:
     shared_ptr<hittable> left;
     shared_ptr<hittable> right;
     aabb bbox;
+
+    static inline bool box_compare(const shared_ptr<hittable> a, const shared_ptr<hittable> b, int axis_index) {
+        return a->bounding_box().axis(axis_index).min < b->bounding_box().axis(axis_index).min;
+    }
+
+    static inline bool box_x_compare(const shared_ptr<hittable> a, const shared_ptr<hittable> b) {
+        return box_compare(a, b, 0);
+    }
+
+    static inline bool box_y_compare(const shared_ptr<hittable> a, const shared_ptr<hittable> b) {
+        return box_compare(a, b, 1);
+    }
+
+    static inline bool box_z_compare(const shared_ptr<hittable> a, const shared_ptr<hittable> b) {
+        return box_compare(a, b, 2);
+    }
 };
 
 #endif//__BHV_H__
