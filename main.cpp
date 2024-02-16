25128b09135afd30150c33ff42883ae0c3a862ff
Borin Ouch <borin.ouch@gmail.com>
2024-02-15 22:47:04 -0800

    Increase image width

--- main.cpp
+++ main.cpp
@@ -113,7 +113,7 @@ int main(int argc, char* argv[]) {
 
     camera cam;
     cam.aspect_ratio = 16.0 / 9.0;
-    cam.image_width = 400;
+    cam.image_width = 1024;
     cam.samples_per_pixel = 100;
     cam.max_depth = 50;
     cam.vfov = 20;
