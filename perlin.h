e4a5cc242c6115377219ae9f58fb5b4653de12d9
Borin Ouch <borin.ouch@gmail.com>
2024-02-17 15:05:35 -0800

    Smooth perlin noise

--- perlin.h
+++ perlin.h
@@ -25,11 +25,29 @@ public:
     }
 
     double noise(const point3& p) const {
-        auto i = static_cast<int>(4 * p.x()) & 255;
-        auto j = static_cast<int>(4 * p.y()) & 255;
-        auto k = static_cast<int>(4 * p.z()) & 255;
+        auto u = p.x() - floor(p.x());
+        auto v = p.y() - floor(p.y());
+        auto w = p.z() - floor(p.z());
 
-        return ranfloat[perm_x[i] ^ perm_y[j] ^ perm_z[k]];
+        auto i = static_cast<int>(floor(p.x()));
+        auto j = static_cast<int>(floor(p.y()));
+        auto k = static_cast<int>(floor(p.z()));
+
+        double c[2][2][2];
+
+        for (int di = 0; di < 2; di += 1) {
+            for (int dj = 0; dj < 2; dj += 1) {
+                for (int dk = 0; dk < 2; dk += 1) {
+                    c[di][dj][dk] = ranfloat[
+                        perm_x[(i + di) & 255] ^
+                        perm_y[(j + dj) & 255] ^
+                        perm_z[(k + dk) & 255]
+                    ];
+                }
+            }
+        }
+
+        return trilinear_interp(c, u, v, w);
     }
 
 private:
@@ -58,6 +76,23 @@ private:
             p[i] = p[target];
             p[target] = tmp;
         }
+    }
+
+    static double trilinear_interp(double c[2][2][2], double u, double v, double w) {
+        double accum = 0.0;
+
+        for (int i = 0; i < 2; i += 1) {
+            for (int j = 0; j < 2; j += 1) {
+                for (int k = 0; k < 2; k += 1) {
+                    accum += (i*u + (1-i) * (1-u)) *
+                             (j*v + (1-j) * (1-v)) *
+                             (k*w + (1-k) * (1-w)) *
+                             c[i][j][k];
+                }
+            }
+        }
+
+        return accum;
     }
 };
 
