546a65f1636c2cc273945e86750f8a74e8155509
Borin Ouch <borin.ouch@gmail.com>
2024-02-20 23:33:21 -0800

    Add rotate_y hittable

--- hittable.h
+++ hittable.h
@@ -74,4 +74,82 @@ private:
     aabb bbox;
 };
 
+class rotate_y : public hittable {
+public:
+    rotate_y(shared_ptr<hittable> p, double angle) : object(p) {
+        auto radians = degrees_to_radians(angle);
+        sin_theta = sin(radians);
+        cos_theta = cos(radians);
+        bbox = object->bounding_box();
+
+        point3 min(infinity, infinity, infinity);
+        point3 max(-infinity, -infinity, -infinity);
+
+        for (int i = 0; i < 2; i += 1) {
+            for (int j = 0; j < 2; j += 1) {
+                for (int k = 0; k < 2; k += 1) {
+                    auto x = i * bbox.x.max + (1 - i) * bbox.x.min;
+                    auto y = j * bbox.y.max + (1 - j) * bbox.y.min;
+                    auto z = k * bbox.z.max + (1 - k) * bbox.z.min;
+
+                    auto new_x = cos_theta * x + sin_theta * z;
+                    auto new_z = -sin_theta * x + cos_theta * z;
+
+                    vec3 tester(new_x, y, new_z);
+                    for (int c = 0; c < 3; c += 1) {
+                        min[c] = fmin(min[c], tester[c]);
+                        max[c] = fmax(max[c], tester[c]);
+                    }
+                }
+            }
+        }
+    }
+
+    bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
+        // convert from world to object space
+        auto origin = r.origin();
+        auto direction = r.direction();
+
+        origin[0] = cos_theta * r.origin()[0] - sin_theta * r.origin()[2];
+        origin[2] = sin_theta * r.origin()[0] + cos_theta * r.origin()[2];
+
+        direction[0] = cos_theta * r.direction()[0] - sin_theta * r.direction()[2];
+        direction[2] = sin_theta * r.direction()[0] + cos_theta * r.direction()[2];
+
+        ray rotated_r(origin, direction, r.time());
+
+        if (!object->hit(rotated_r, ray_t, rec)) {
+            return false;
+        }
+
+        // convert from object to world space
+        auto p = rec.p;
+        p[0] = cos_theta * rec.p[0] + sin_theta * rec.p[2];
+        p[2] = -sin_theta * rec.p[0] + cos_theta * rec.p[2];
+
+        auto normal = rec.normal;
+        normal[0] = cos_theta * rec.normal[0] + sin_theta * rec.normal[2];
+        normal[2] = -sin_theta * rec.normal[0] + cos_theta * rec.normal[2];
+
+        rec.p = p;
+        rec.normal = normal;
+
+        return true;
+    }
+
+    aabb bounding_box() const override {
+        return bbox;
+    }
+
+    void draw() const override {
+        // object->draw();
+    }
+
+private:
+    shared_ptr<hittable> object;
+    double sin_theta;
+    double cos_theta;
+    aabb bbox;
+};
+
 #endif// __HITTABLE_H__
