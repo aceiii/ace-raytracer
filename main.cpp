3ceea915fb470478c746d6bb8e4b43d4bd7005c7
Borin Ouch <borin.ouch@gmail.com>
2024-02-20 23:37:21 -0800

    Use rotation for boxes in cornell scene

--- main.cpp
+++ main.cpp
@@ -227,8 +227,15 @@ scene_info cornell_box() {
     scene.world.add(make_shared<quad>(point3(555, 555, 555), vec3(-555, 0, 0), vec3(0, 0, -555), white));
     scene.world.add(make_shared<quad>(point3(0, 0, 555), vec3(555, 0, 0), vec3(0, 555, 0), white));
 
-    scene.world.add(box(point3(130, 0, 65), point3(295, 165, 230), white));
-    scene.world.add(box(point3(265, 0, 295), point3(430, 330, 460), white));
+    shared_ptr<hittable> box1 = box(point3(0, 0, 0), point3(165, 330, 165), white);
+    box1 = make_shared<rotate_y>(box1, 15);
+    box1 = make_shared<translate>(box1, vec3(265, 0, 295));
+    scene.world.add(box1);
+
+    shared_ptr<hittable> box2 = box(point3(0, 0, 0), point3(165, 165, 165), white);
+    box2 = make_shared<rotate_y>(box2, -18);
+    box2 = make_shared<translate>(box2, vec3(130, 0, 65));
+    scene.world.add(box2);
 
     scene.vfov = 40;
     scene.lookfrom = point3(278, 278, -800);
